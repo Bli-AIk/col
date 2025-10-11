5e3bf5ffb1b7fa74dd422d026cac1afc6e76ee7d
Bli_Aik <haikun2333@126.com>
2025-10-11 16:36:30 +0800

    refactor(token): reformat token enum with one variant per line

--- src/token.rs
+++ src/token.rs
@@ -4,94 +4,175 @@ use logos::Logos;
 #[logos(skip r"[ \t\n]+")]
 pub(crate) enum Token {
     // Keywords
-    #[token("var")] Var,
-    #[token("globalvar")] GlobalVar,
-    #[token("localvar")] LocalVar,
-    #[token("enum")] Enum,
-    #[token("function")] Function,
-    #[token("if")] If,
-    #[token("else")] Else,
-    #[token("switch")] Switch,
-    #[token("case")] Case,
-    #[token("default")] Default,
-    #[token("for")] For,
-    #[token("while")] While,
-    #[token("do")] Do,
-    #[token("break")] Break,
-    #[token("continue")] Continue,
-    #[token("return")] Return,
-    #[token("true")] True,
-    #[token("false")] False,
-    #[token("undefined")] Undefined,
-    #[token("null")] Null,
-    #[token("repeat")] Repeat,
-    #[token("with")] With,
-    #[token("self")] Self_,
-    #[token("other")] Other,
-    #[token("and")] AndWord,
-    #[token("or")] OrWord,
-    #[token("not")] NotWord,
-    #[token("global")] Global,
-    #[token("all")] All,
-    #[token("noone")] Noone,
-    #[token("exit")] Exit,
-    #[token("begin")] Begin,
-    #[token("end")] End,
-    #[token("try")] Try,
-    #[token("catch")] Catch,
-    #[token("finally")] Finally,
-    #[token("throw")] Throw,
-    #[token("new")] New,
-    #[token("delete")] Delete,
-    #[token("constructor")] Constructor,
-    #[token("static")] Static,
+    #[token("var")]
+    Var,
+    #[token("globalvar")]
+    GlobalVar,
+    #[token("localvar")]
+    LocalVar,
+    #[token("enum")]
+    Enum,
+    #[token("function")]
+    Function,
+    #[token("if")]
+    If,
+    #[token("else")]
+    Else,
+    #[token("switch")]
+    Switch,
+    #[token("case")]
+    Case,
+    #[token("default")]
+    Default,
+    #[token("for")]
+    For,
+    #[token("while")]
+    While,
+    #[token("do")]
+    Do,
+    #[token("break")]
+    Break,
+    #[token("continue")]
+    Continue,
+    #[token("return")]
+    Return,
+    #[token("true")]
+    True,
+    #[token("false")]
+    False,
+    #[token("undefined")]
+    Undefined,
+    #[token("null")]
+    Null,
+    #[token("repeat")]
+    Repeat,
+    #[token("with")]
+    With,
+    #[token("self")]
+    Self_,
+    #[token("other")]
+    Other,
+    #[token("and")]
+    AndWord,
+    #[token("or")]
+    OrWord,
+    #[token("not")]
+    NotWord,
+    #[token("global")]
+    Global,
+    #[token("all")]
+    All,
+    #[token("noone")]
+    Noone,
+    #[token("exit")]
+    Exit,
+    #[token("begin")]
+    Begin,
+    #[token("end")]
+    End,
+    #[token("try")]
+    Try,
+    #[token("catch")]
+    Catch,
+    #[token("finally")]
+    Finally,
+    #[token("throw")]
+    Throw,
+    #[token("new")]
+    New,
+    #[token("delete")]
+    Delete,
+    #[token("constructor")]
+    Constructor,
+    #[token("static")]
+    Static,
 
     // Operators
     // See: https://manual.gamemaker.io/monthly/en/#t=GameMaker_Language%2FGML_Overview%2FExpressions_And_Operators.htm&rhsearch=globalvar
-    #[token("+=")] PlusEqual,
-    #[token("-=")] MinusEqual,
-    #[token("*=")] StarEqual,
-    #[token("/=")] SlashEqual,
-    #[token("%=")] PercentEqual,
-    #[token("==")] EqualEqual,
-    #[token("!=")] NotEqual,
-    #[token("<=")] LessEqual,
-    #[token(">=")] GreaterEqual,
-    #[token("<<")] ShiftLeft,
-    #[token(">>")] ShiftRight,
-    #[token("++")] PlusPlus,
-    #[token("--")] MinusMinus,
-    #[token("&&")] And,
-    #[token("||")] Or,
-    #[token("+")] Plus,
-    #[token("-")] Minus,
-    #[token("*")] Star,
-    #[token("/")] Slash,
-    #[token("%")] Percent,
-    #[token("=")] Equal,
-    #[token("<")] Less,
-    #[token(">")] Greater,
-    #[token("!")] Not,
-    #[token(";")] Semicolon,
-    #[token(",")] Comma,
-    #[token(".")] Dot,
-    #[token("(")] LeftParen,
-    #[token(")")] RightParen,
-    #[token("{")] LeftBrace,
-    #[token("}")] RightBrace,
-    #[token("[")] LeftBracket,
-    #[token("]")] RightBracket,
-    #[token("&")] BitAnd,
-    #[token("|")] BitOr,
-    #[token("^")] BitXor,
-    #[token("~")] BitNot,
-    #[token("?")] Question,
-    #[token(":")] Colon,
+    #[token("+=")]
+    PlusEqual,
+    #[token("-=")]
+    MinusEqual,
+    #[token("*=")]
+    StarEqual,
+    #[token("/=")]
+    SlashEqual,
+    #[token("%=")]
+    PercentEqual,
+    #[token("==")]
+    EqualEqual,
+    #[token("!=")]
+    NotEqual,
+    #[token("<=")]
+    LessEqual,
+    #[token(">=")]
+    GreaterEqual,
+    #[token("<<")]
+    ShiftLeft,
+    #[token(">>")]
+    ShiftRight,
+    #[token("++")]
+    PlusPlus,
+    #[token("--")]
+    MinusMinus,
+    #[token("&&")]
+    And,
+    #[token("||")]
+    Or,
+    #[token("+")]
+    Plus,
+    #[token("-")]
+    Minus,
+    #[token("*")]
+    Star,
+    #[token("/")]
+    Slash,
+    #[token("%")]
+    Percent,
+    #[token("=")]
+    Equal,
+    #[token("<")]
+    Less,
+    #[token(">")]
+    Greater,
+    #[token("!")]
+    Not,
+    #[token(";")]
+    Semicolon,
+    #[token(",")]
+    Comma,
+    #[token(".")]
+    Dot,
+    #[token("(")]
+    LeftParen,
+    #[token(")")]
+    RightParen,
+    #[token("{")]
+    LeftBrace,
+    #[token("}")]
+    RightBrace,
+    #[token("[")]
+    LeftBracket,
+    #[token("]")]
+    RightBracket,
+    #[token("&")]
+    BitAnd,
+    #[token("|")]
+    BitOr,
+    #[token("^")]
+    BitXor,
+    #[token("~")]
+    BitNot,
+    #[token("?")]
+    Question,
+    #[token(":")]
+    Colon,
 
     // Literals
-    #[regex(r"[a-zA-Z_][a-zA-Z0-9_]*")] Identifier,
-    #[regex(r#""([^"\\]|\\.)*""#)] String,
-    #[regex(r"\d+(\.\d+)?")] Number,
-
+    #[regex(r"[a-zA-Z_][a-zA-Z0-9_]*")]
+    Identifier,
+    #[regex(r#""([^"\\]|\\.)*""#)]
+    String,
+    #[regex(r"\d+(\.\d+)?")]
+    Number,
 }
-
